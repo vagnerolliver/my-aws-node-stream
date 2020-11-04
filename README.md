799827daf4024fab44922bfe6f9a65f81d8a32ed
Vágner Oliveira <vagner.oliveira@zenvia.com>
2020-11-04 08:42:09 -0300

    - feat: config localStack docker-compose - chore: create s3 bucket localStack - chore: create file in s3 bucket localStack - add: nodemon, serverless, serverless-offline, serverless-mocha-plugin

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my-aws-node-stream
 Processando arquivos sob demanda com Node.js Streams
+
+ 
+#Setting up localstack
+https://lobster1234.github.io/2017/04/05/working-with-localstack-command-line/ 
---
+++ tooling-s3-sqs/scripts/s3/create-bucket.sh
@@ -0,0 +1,7 @@
+#bash scripts/s3/create-bucket.sh meu-bucket
+BUCKET_NAME=$1
+aws \
+    --endpoint-url=http://localhost:4566 s3 mb s3://$BUCKET_NAME
+ 
+aws \
+    --endpoint-url=http://localhost:4566 s3 ls
