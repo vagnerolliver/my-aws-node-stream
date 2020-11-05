bb736bc1d4c14b7c2c7ae95a3a3f3e0c5ba714ad
Vágner Oliveira <vagner.oliveira@zenvia.com>
2020-11-05 08:41:05 -0300

    - feat: livereload dockerfile and serverless localstack - chore: add docker-compose ap - chore: config AWS.Endpoint enviroments docker-compose

--- tooling-s3-sqs/handler.js
+++ tooling-s3-sqs/handler.js
@@ -1,6 +1,19 @@
 'use strict';
+const AWS = require('aws-sdk')
+
+const host = process.env.LOCALSTACK_HOST || 'localhost'
+const s3port = process.env.S3_PORT || '4566'
+const s3config = {
+  s3ForcePathStyle: true,
+  endpoint: new AWS.Endpoint(
+    `http://${host}:${s3port}`
+  )
+}
+const S3 = new AWS.S3(s3config)
 
 module.exports.hello = async event => {
+  const allBuckets = await S3.listBuckets().promise()
+  console.log('#################### allBuckets', allBuckets)
   return {
     statusCode: 200,
     body: JSON.stringify(
